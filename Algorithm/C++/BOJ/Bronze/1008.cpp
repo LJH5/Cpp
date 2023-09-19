aad16abf62255c2605e3ae54c58d7e99d6d36c6f
LeeJunho <sigma6317@gmail.com>
2023-09-19 16:59:24 +0900

    make cpp repository

---
+++ Algorithm/C++/BOJ/Bronze/1008.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+
+    double a, b;
+    cin >> a >> b;
+
+    cout.precision(12);
+    cout << fixed;
+    cout << a / b << "\n";
+
+    return 0;
+}
---
+++ Algorithm/C++/BOJ/Bronze/27866.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string S;
+    cin >> S;
+    int i;
+    cin >> i;
+
+    cout << S[i - 1];
+
+    return 0;
+}
