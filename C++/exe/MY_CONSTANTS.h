38848302ce11f439c883ae6cc45278e7b68bff1b
LJH <sigma6317@gmail.com>
2023-10-13 22:36:08 +0900

    constants

---
+++ C++/exe/MY_CONSTANTS.h
@@ -0,0 +1,7 @@
+#pragam once // 헤더 가드, 헤더 파일이 단일 컴파일에 한 번만 포함되도록 하는 방법
+
+namespace constants {
+	constexpr double pi(3.141592);
+	constexpr double avogadro(6.0221413e23);
+	constexpr double moon_gravity(9.8 / 6.0);
+}
