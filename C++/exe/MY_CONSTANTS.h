7efa94ba68d876e8418d4529f7064fbdc7e5e590
LJH <sigma6317@gmail.com>
2023-10-14 22:58:34 +0900

    const

--- C++/exe/MY_CONSTANTS.h
+++ C++/exe/MY_CONSTANTS.h
@@ -1,6 +1,9 @@
+// const 변수를 한번에 모아놓은 헤더 파일
 #pragam once // 헤더 가드, 헤더 파일이 단일 컴파일에 한 번만 포함되도록 하는 방법
 
+// namespace 선언
 namespace constants {
+	// constexpr: 컴파일 단계에서 값을 계산
 	constexpr double pi(3.141592);
 	constexpr double avogadro(6.0221413e23);
 	constexpr double moon_gravity(9.8 / 6.0);
--- C++/exe/Source1.cpp
+++ C++/exe/Source1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-#define PRICE_PER_ITEM 30
+// define: c 스타일 매크로, cpp에서 사용비추, 디버깅이 힘듦, 파일 전체에 영향
+#define PRICE_PER_ITEM 30 
 
 using namespace std;
 
 int main() {
     int num_item = 10;
-    int price = num_item * PRICE_PER_ITEM;
+    int price = num_item * PRICE_PER_ITEM; 
     cout << price << "\n";
 
     return 0;
