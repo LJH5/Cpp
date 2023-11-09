475b7fdd6b64f8636203e492de269f6d32516612
LJH <sigma6317@gmail.com>
2023-11-09 21:29:54 +0900

    linker

--- C++/cpp/static_variable/static_global_variable.cpp
+++ C++/cpp/static_variable/static_global_variable.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+// 아래의 방법 사용 시 중복 정의, 컴파일 시간 증가, 의존성 문제, 코드 가독성 감소 등의 단점
+// #include "static_global_variable_test.cpp"
 using namespace std;
 
 static int s_g_value = 10;
 int g_value = 11;
 // forward declaration
-void doSomething();
+extern void doSomething();
 
 int main() {
     cout << s_g_value << "\n";
