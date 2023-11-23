3649ad7fb534adf20571e22024935583245fb368
LJH <sigma6317@gmail.com>
2023-11-23 23:16:10 +0900

    operator

---
+++ C++/cpp/operator/arithmetic_operator_03.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 7;
+    int y = 4;
+
+    int z = x;
+    z += y; // z = z + y;
+}
---
+++ C++/cpp/operator/operator_precedenc_associativity_01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 연산자 우선순위 표를 참고할 것
+    // ()를 사용하면 우선순위 문제는 대부분 해결됨
+    int x = (4 + 2) * 3;
+
+}
