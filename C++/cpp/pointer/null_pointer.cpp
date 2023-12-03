fc5df83848ca4393bd07b9e4f3019246f92fb0de
LJH <sigma6317@gmail.com>
2023-12-03 11:49:09 +0900

    study_pointer

--- C++/cpp/pointer/null_pointer.cpp
+++ C++/cpp/pointer/null_pointer.cpp
@@ -1,10 +1,14 @@
-# include <iostream>
+#include <iostream>
+#include <cstddef>
 
 using namespace std;
 
 void def(double *ptr)
 {
-     if (ptr != nullptr)
+    // 매개변수로 넘어오는 변수는 재선언 후 복사됨, 메모리주소가 다름
+    cout << "def(): " << &ptr << "\n";
+
+    if (ptr != nullptr)
     {
         cout << *ptr << endl;
     }
@@ -12,18 +16,25 @@ void def(double *ptr)
     {
         cout << "Null Pointer" << endl;
     }
-    
 }
 
 int main()
 {
-    double *ptr{ nullptr };
+    double *ptr{nullptr};
 
     def(ptr);
     def(nullptr);
 
     double d = 3.14;
     def(&d);
+
+    ptr = &d;
+    def(ptr);
+
+    cout << "main(): " << &d << "\n";
+
+    // 매개변수 중 nullptr만 필요로 할 때 사용
+    std::nullptr_t nptr;
 
     return 0;
 }
