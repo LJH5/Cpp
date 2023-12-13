eb42cb51d9ba9c37fd307fc5e381a13e81711658
LJH <sigma6317@gmail.com>
2023-12-13 15:19:44 +0900

    merge

--- Algorithm/BOJ/Bronze/1152.cpp
+++ Algorithm/BOJ/Bronze/1152.cpp
@@ -4,17 +4,16 @@ using namespace std;
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	
+
 	int cnt = 1;
 	string str;
 	getline(cin, str);
 	int str_len = str.length();
-	for (int i = 0; i < str_len; i++) 
+	for (int i = 0; i < str_len; i++)
 		if (str[i] == ' ') cnt++;
-	
-	if (str[0] == ' ')	cnt--;						// Ã³À½¿¡ ºóÄ­
-	if (str[str.length() - 1] == ' ') cnt--;	// ¸¶Áö¸·¿¡ ºóÄ­
-	cout << cnt;
+
+	if (str[0] == ' ')	cnt--;
+	if (str[str.length() - 1] == ' ') cnt--;
 
 	return 0;
 }
