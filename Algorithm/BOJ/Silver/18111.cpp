d36b393aa8988ee7dd93290a474b6445184cd928
LJH <sigma6317@gmail.com>
2024-01-28 23:01:44 +0900

    algo_bog;

---
+++ Algorithm/BOJ/Silver/18111.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, b, num;
+int lend[250001];
+
+// 쌓을 수 있는 지 확인
+void check()
+{
+
+}
+
+// 깍기
+void mod()
+{
+
+}
+
+// 쌓기
+void pile()
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m >> b;
+    for (int i = 0; i < n*m; i++)
+    {
+        cin >> num;
+        lend[i] = num;
+    }
+
+    return 0;
+}
